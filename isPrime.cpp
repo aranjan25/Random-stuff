3ce784fe68952eb87e07e1cfa9f0b327ead40c00
Zark84010 <adityaranjan25@gmail.com>
2018-11-07 03:58:32 +0530

    Update isPrime.cpp

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,9 +1,9 @@
  bool isPrime(int n){
  	int sq=sqrt(n),i;
- 	if(n==1||n%2==0||n%3==0)
- 		return false;
- 	if(n==2)
+ 	if(n==2||n==3)
  		return true;
+  if(n==1||n%2==0||n%3==0)
+ 		return false;
  	for(i=3;i<=sq;i+=2){
  		if(n%i==0)
  			return false;
