0e45a67cf3fb88f15f603cb49e91192b97c7f244
Zark84010 <adityaranjan25@gmail.com>
2018-11-15 01:07:10 +0530

    Update nth_prime.cpp

--- nth_prime.cpp
+++ nth_prime.cpp
@@ -1,3 +1,15 @@
+bool isPrime(int n){
+ 	int sq=sqrt(n),i;
+ 	if(n==2||n==3)
+ 		return true;
+  if(n==1||n%2==0||n%3==0)
+ 		return false;
+ 	for(i=3;i<=sq;i+=2){
+ 		if(n%i==0)
+ 			return false;
+ 	}
+ 	return true;
+}
 int p(int n){                   //nth prime
   int k;
   if(n==1)
