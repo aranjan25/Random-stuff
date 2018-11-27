766a3ab2c501cfcd80299b80e69a176a013a5749
Zark84010 <adityaranjan25@gmail.com>
2018-11-27 12:31:29 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,7 +4,10 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 #define int long long
- 
+
+typedef vector<int> vi;
+#define REP(i,a,b) for (i = a; i <= b; i++)
+
 const int N=1e5+5;
  
 int32_t main()
