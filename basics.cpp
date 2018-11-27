ac6713e52f449518f82b766bcd05cda727b13633
Zark84010 <adityaranjan25@gmail.com>
2018-11-27 08:09:10 +0530

    Update and rename input_basics.cpp to basics.cpp

--- basics.cpp
+++ basics.cpp
@@ -10,3 +10,7 @@ while(cin>>x){
 //Inputting and outputting using files
 freopen("input.txt", "r", stdin);
 freopen("output.txt", "w", stdout);
+
+//float data-types
+printf("%.9f\n",x); //prints x with 9 decimal places
+printf("%.20f\n",x);  //prints x with 20 decimal places etc.
