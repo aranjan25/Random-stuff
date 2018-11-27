1e9e45c9f5d97d9464fc2572d98aaca1075c6dcd
Zark84010 <adityaranjan25@gmail.com>
2018-11-27 08:11:55 +0530

    Update basics.cpp

--- basics.cpp
+++ basics.cpp
@@ -14,3 +14,9 @@ freopen("output.txt", "w", stdout);
 //float data-types
 printf("%.9f\n",x); //prints x with 9 decimal places
 printf("%.20f\n",x);  //prints x with 20 decimal places etc.
+//Note that float and double data types are susceptible to rounding errors.
+
+//Use this to compare two float types with required precision (for example 10^(-9))
+if (abs(a-b)<1e-9) {
+  // a and b are equal
+}
