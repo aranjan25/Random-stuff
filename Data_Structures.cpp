1c27a12e6a2a4b5170f0f748dfbd8689d66c612c
Zark84010 <adityaranjan25@gmail.com>
2018-11-27 12:39:23 +0530

    Update Data_Structures.cpp

--- Data_Structures.cpp
+++ Data_Structures.cpp
@@ -1,1 +1,7 @@
+string s;
+getline(cin,s);   //inputting string
+string s1,s2;
 
+s=s1+s2   //Combine strings
+
+string a = s.substr(k,l);  //Substring of 's' starting at index 'k' and of length 'l'
