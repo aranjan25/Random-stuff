32df62e49809ddbbe5fdd37489b86a4359f9b18a
Zark84010 <adityaranjan25@gmail.com>
2019-01-03 18:34:28 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,10 +4,10 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 #define int long long
-
-typedef vector<int> vi;
+#define max(a,b) (((a)>(b)))?(a):(b)
+#define min(a,b) (((a)<(b)))?(a):(b)
 #define PB push_back
-#define REP(i,a,b) for(i=a; i<b; i++)
+#define rep(i,a,b) for(i=a; i<b; i++)
 
 const int N=1e5+5;
  
