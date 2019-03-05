1d0e0945b4d3f9f899e267c4cd9a781e8b762dcf
Zark84010 <adityaranjan25@gmail.com>
2019-03-05 15:44:38 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 #define int long long
+#define pb push_back
+#define rep1(i,a,b) for(i=a;i<b;i++)
+#define rep0(i,c) rep1(i,0,c)
+#define rep(i) rep0(i,n)
 #define max(a,b) (((a)>(b))?(a):(b))
+#define max0(a,b,c) (max(max(a,b),c))
 #define min(a,b) (((a)<(b))?(a):(b))
-#define PB push_back
-#define rep(i,a,b) for(i=a; i<b; i++)
+#define min0(a,b,c) (min(min(a,b),c))
 
-const int N=1e5+5;
- 
-int32_t main()
-{
-	IOS;
- 	 
-	return 0;
+typedef vector<int> vi;
+
+const int N=1e9+7;
+
+int32_t main(){
+  IOS;
+  int t
+  cin>>t;
+  while(t--){
+
+  }
+  return 0;
 }
