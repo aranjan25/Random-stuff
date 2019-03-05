c99bc0612f2ef530dca5495a2502162b982bca19
Zark84010 <adityaranjan25@gmail.com>
2019-03-05 15:37:24 +0530

    Update isPrime.cpp

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,13 +1,13 @@
- bool isPrime(int n){
- 	int i;
- 	if(n==2||n==3)
- 		return true;
-  if(n==1||n%2==0||n%3==0)
- 		return false;
- 	for(i=3;i*i<=n;i+=2){
- 		if(n%i==0)
- 			return false;
- 	}
- 	return true;
- }
- 
+//Sieve of Eratosthenes. Time Complexity O(n log log n)
+bool isPrime(int n){
+  int i, j;
+  vector<bool> is_p(n+1, true);
+  is_p[0]=is_p[1]=false;
+  is_p[2]=true;
+  for(i=2;i<=n/2;i++) is_p[2*i]=false;
+  for(i=3;i*i<=n;i+=2){
+    if(is_p[i])
+      for(j=i*i;j<=n;j+=i) is_p[j]=false;
+  }
+  return is_p[n];
+}
