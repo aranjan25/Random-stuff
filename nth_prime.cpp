1b490cb029643e7e2995289f0be25cff60a0b7c0
Zark84010 <adityaranjan25@gmail.com>
2019-03-05 15:42:16 +0530

    Update nth_prime.cpp

--- nth_prime.cpp
+++ nth_prime.cpp
@@ -1,29 +1,24 @@
 bool isPrime(int n){
- 	int i;
- 	if(n==2||n==3)
- 		return true;
-  if(n==1||n%2==0||n%3==0)
- 		return false;
- 	for(i=3;i*i<=n;i+=2){
- 		if(n%i==0)
- 			return false;
- 	}
- 	return true;
-}
-int p(int n){                   //nth prime
-  int k;
-  if(n==1)
-    return 2;
-  if(n==2)
-    return 3;
-  else{
-    k=p(n-1)+2;
-    while(1){
-      if(isPrime(k))
-        break;
-      else
-        k+=2;
-    }
-    return k;
+  int i, j;
+  vector<bool> is_p(n+1, true);
+  is_p[0]=is_p[1]=false;
+  is_p[2]=true;
+  for(i=2;i<=n/2;i++) is_p[2*i]=false;
+  for(i=3;i*i<=n;i+=2){
+    if(is_p[i])
+      for(j=i*i;j<=n;j+=i) is_p[j]=false;
   }
+  return is_p[n];
+}
+
+int p(int n){
+ if(n==1)
+  return 2;
+ if(n==2)
+  return 3;
+ int k=p(n-1)+2;
+ while(1){
+  if(isPrime(k)) return k;
+  else k+=2;
+ }
 }
