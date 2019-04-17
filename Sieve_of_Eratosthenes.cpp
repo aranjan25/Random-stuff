abc43cbf38737cf983003a433363e15f81504be8
Zark84010 <adityaranjan25@gmail.com>
2019-04-17 19:39:23 +0530

    Update Sieve_of_Eratosthenes.cpp

--- Sieve_of_Eratosthenes.cpp
+++ Sieve_of_Eratosthenes.cpp
@@ -1,7 +1,8 @@
 //Sieve of Eratosthenes. Time Complexity O(n log log n)
-bool isPrime(int n){
+vector<bool> is_p(n+1, true);
+
+void sieve(int n){
   int i, j;
-  vector<bool> is_p(n+1, true);
   is_p[0]=is_p[1]=false;
   is_p[2]=true;
   for(i=2;i<=n/2;i++) is_p[2*i]=false;
@@ -9,5 +10,4 @@ bool isPrime(int n){
     if(is_p[i])
       for(j=i*i;j<=n;j+=i) is_p[j]=false;
   }
-  return is_p[n];
 }
