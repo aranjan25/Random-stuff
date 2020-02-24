be39e9dda8255d343914ee7bd43fe27b9b4fe41b
Aditya Ranjan <adityaranjan25@gmail.com>
2020-02-24 15:37:26 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,35 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 #define endl "\n"
 
-#define sz(a) int((a).size())
+#define sz(a) ( (int)(a).size() )
 #define all(v) (v).begin(), (v).end()
-#define tr(c,it) for(decltype((c).begin()) it=(c).begin(); it!=(c).end(); it++)
-#define pres(c,val) ((c).find(val) != (c).end())
-#define cpres(c,val) (find(all(c), val) != (c).end())
+#define tr(c,it) for(auto it = (c).begin(); it != (c).end(); it++)
+#define pres(c,val) ((c).find(val) != (c).end())    //
+#define cpres(c,val) (find(all(c), val) != (c).end())   //
 
-#define PB push_back
-#define MP make_pair
+#define pb push_back
+#define mp make_pair
 #define F first
 #define S second
 
-#define REP(i,a,b) for(int i=a;i<b;i++)
-#define rep(i) REP(i,0,n)
-
-#define MAX(a,b,c) (max(max(a,b),c))
-#define MIN(a,b,c) (min(min(a,b),c))
+#define REP(i, a, b) for(int i = a; i < b; i++)
+#define rep(i) REP(i, 0, n)
 
 typedef vector<int> vi;
-typedef pair<int,int> pii;
+typedef pair<int, int> pii;
 typedef vector<int>::iterator vit;
 
-const int N=1e9+7;
+const int N = 1e9 + 7;
+
+void solve(void) {
+
+}
 
 int32_t main(){
-    IOS;
-    
+    fastio;
+    int t;
+    cin >> t;
+    while(t--) solve();
     return 0;
 }
